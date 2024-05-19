eb89675b8019814cc6581a901d1354387e15909d
Oliwier Juncewicz <oli.junc@gmail.com>
2024-05-19 18:08:22 +0200

    Updated BookApiService.kt

--- app/src/main/java/com/junclabs/bookshelf/network/BookApiService.kt
+++ app/src/main/java/com/junclabs/bookshelf/network/BookApiService.kt
@@ -3,10 +3,11 @@ package com.junclabs.bookshelf.network
 import com.junclabs.bookshelf.model.BookPhoto
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface BookApiService {
-    @GET("volumes?q=q")
-    suspend fun getPhotos(): List<BookPhoto>
+    @GET("volumes")
+    suspend fun getPhotos(@Query("q") query: String): List<BookPhoto>
     @GET("volumes/{id}")
     suspend fun getPhoto(@Path("id") id: Int): BookPhoto
 }
