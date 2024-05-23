1882a17e054835b1d7cce594bea75a4a087cc73e
Oliwier Juncewicz <oli.junc@gmail.com>
2024-05-23 20:49:45 +0200

    Updated BookRepository.kt

--- app/src/main/java/com/junclabs/bookshelf/data/BookRepository.kt
+++ app/src/main/java/com/junclabs/bookshelf/data/BookRepository.kt
@@ -5,12 +5,12 @@ import com.junclabs.bookshelf.network.BookApiService
 
 interface BookRepository {
     suspend fun getPhotos(query: String): List<BookPhoto>
-    suspend fun getPhoto(id: Int): BookPhoto
+    suspend fun getPhoto(id: String): BookPhoto
 }
 
 class NetworkBookRepository(
     private val bookApiService: BookApiService
 ) : BookRepository {
     override suspend fun getPhotos(query: String): List<BookPhoto> = bookApiService.getPhotos(query = query)
-    override suspend fun getPhoto(id: Int): BookPhoto = bookApiService.getPhoto(id = id)
+    override suspend fun getPhoto(id: String): BookPhoto = bookApiService.getPhoto(id = id)
 }
