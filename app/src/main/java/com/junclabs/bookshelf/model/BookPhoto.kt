12154130d1cdba77980500de65b416e0b1db86aa
Oliwier Juncewicz <oli.junc@gmail.com>
2024-05-28 19:40:46 +0200

    Updated BookPhoto class to use the volumeInfo class

--- app/src/main/java/com/junclabs/bookshelf/model/BookPhoto.kt
+++ app/src/main/java/com/junclabs/bookshelf/model/BookPhoto.kt
@@ -1,12 +1,13 @@
 package com.junclabs.bookshelf.model
 
+import com.junclabs.bookshelf.network.VolumeInfo
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class BookPhoto(
-    @SerialName(value = "thumbnail")
-    val image: String,
     @SerialName(value = "id")
-    val id: String
+    val id: String,
+    @SerialName(value = "volumeinfo")
+    val volumeInfo: VolumeInfo
 )
