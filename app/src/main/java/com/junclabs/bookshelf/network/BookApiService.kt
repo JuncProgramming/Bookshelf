c38aae6793d1fb103f12805d9d1f78272c971776
Oliwier Juncewicz <oli.junc@gmail.com>
2024-06-01 21:55:38 +0200

    Updated BookApiService.kt with a query

--- app/src/main/java/com/junclabs/bookshelf/network/BookApiService.kt
+++ app/src/main/java/com/junclabs/bookshelf/network/BookApiService.kt
@@ -7,7 +7,7 @@ import retrofit2.http.Query
 
 interface BookApiService {
     @GET("volumes")
-    suspend fun getPhotos(@Query("q") query: String): List<BookPhoto>
+    suspend fun getPhotos(@Query("q") query: String = "juncewicz"): List<BookPhoto>
     @GET("volumes/{id}")
-    suspend fun getPhoto(@Path("id") id: String): BookPhoto
+    suspend fun getPhoto(@Path("id") id: String = "94vftwEACAAJ"): BookPhoto
 }
