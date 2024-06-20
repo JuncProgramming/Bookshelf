f7e8b057be33c8601fbe6c05b5b113edc946c437
Oliwier Juncewicz <oli.junc@gmail.com>
2024-06-20 23:22:10 +0200

    Deleted some redundant classes and functions from BookRepository.kt

--- app/src/main/java/com/junclabs/bookshelf/data/BookRepository.kt
+++ app/src/main/java/com/junclabs/bookshelf/data/BookRepository.kt
@@ -5,12 +5,10 @@ import com.junclabs.bookshelf.network.BookApiService
 
 interface BookRepository {
     suspend fun getPhotos(query: String): BookVolumes
-    suspend fun getPhoto(id: String): BookVolumes
 }
 
 class NetworkBookRepository(
     private val bookApiService: BookApiService
 ) : BookRepository {
     override suspend fun getPhotos(query: String): BookVolumes = bookApiService.getPhotos(query = query)
-    override suspend fun getPhoto(id: String): BookVolumes = bookApiService.getPhoto(id = id)
 }
