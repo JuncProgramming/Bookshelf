e1c525248dbf796323802ab4ff336ee6e566f84e
Oliwier Juncewicz <oli.junc@gmail.com>
2024-06-25 23:53:24 +0200

    Made changes to Screens.kt, so the user can type the query himself, resulting in option to search the data by the user. Also added some strings resources.

--- app/src/main/java/com/junclabs/bookshelf/ui/Screens.kt
+++ app/src/main/java/com/junclabs/bookshelf/ui/Screens.kt
@@ -1,24 +1,41 @@
 package com.junclabs.bookshelf.ui
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
+import com.junclabs.bookshelf.R
 import com.junclabs.bookshelf.model.BookVolumes
 import com.junclabs.bookshelf.model.Item
 
@@ -27,46 +44,78 @@ fun BookCard(book: Item) {
     Card {
         AsyncImage(
             model = ImageRequest.Builder(context = LocalContext.current)
-            .data(book.volumeInfo.imageLinks.thumbnail.replace("http", "https"))
-            .crossfade(true)
-            .build(),
+                .data(book.volumeInfo.imageLinks.thumbnail.replace("http", "https")).crossfade(true)
+                .build(),
             contentDescription = null,
+            alignment = Alignment.Center,
             contentScale = ContentScale.Crop,
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier.fillMaxSize()
         )
     }
 }
 
 @Composable
-fun BookList(
-    books: BookVolumes,
-    modifier: Modifier = Modifier,
-    contentPadding: PaddingValues = PaddingValues(8.dp),
+fun BooksSearchScreen(
+    books: BookVolumes, onBooksSearch: (String) -> Unit
 ) {
-    LazyVerticalGrid(
-        columns = GridCells.Fixed(2),
-        modifier = modifier.padding(horizontal = 8.dp),
-        contentPadding = contentPadding,
-    ) {
-        items(items = books.items) { book ->
-            BookCard(book = book)
+    val focusManager = LocalFocusManager.current
+    var query by rememberSaveable { mutableStateOf("") }
+    Column {
+        OutlinedTextField(
+            value = query,
+            onValueChange = { query = it },
+            singleLine = true,
+            placeholder = {
+                Text(text = stringResource(R.string.search))
+            },
+            keyboardOptions = KeyboardOptions(
+                imeAction = ImeAction.Search
+            ),
+            keyboardActions = KeyboardActions(onSearch = {
+                focusManager.clearFocus()
+                onBooksSearch(query)
+            }),
+            leadingIcon = { Icon(imageVector = Icons.Filled.Search, contentDescription = null) },
+            trailingIcon = {
+                Icon(imageVector = Icons.Filled.Clear,
+                    contentDescription = null,
+                    Modifier.clickable { query = "" })
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 8.dp, end = 8.dp, top = 8.dp)
+        )
+        LazyVerticalGrid(
+            columns = GridCells.Adaptive(150.dp),
+            modifier = Modifier.fillMaxWidth(),
+            contentPadding = PaddingValues(8.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            horizontalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            items(items = books.items) { book ->
+                BookCard(book = book)
+            }
         }
     }
 }
 
 @Composable
 fun LoadingScreen() {
-    Column(verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
-        Text(text = "Ladowanie...")
+    Column(
+        verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = stringResource(R.string.loading))
     }
 }
 
 @Composable
 fun ErrorScreen(retryAction: () -> Unit) {
-    Column(verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
-        Text(text = "Dupa", modifier = Modifier.padding(16.dp))
+    Column(
+        verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = stringResource(R.string.error), modifier = Modifier.padding(16.dp))
         Button(onClick = retryAction) {
-            Text("Spróbuj ponownie")
+            Text(stringResource(R.string.try_again))
         }
     }
 }
